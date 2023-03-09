d2e3aff63f38c2c478bf5dc76c5d81595e04ddfa
danieletognetti01 <98704905+danieletognetti01@users.noreply.github.com>
2023-03-09 19:10:44 +0100

    Update LoRa_TX_and_serial.ino

--- LoRa_TX_and_serial.ino
+++ LoRa_TX_and_serial.ino
@@ -2,7 +2,7 @@
 #include <RH_RF95.h>
 
 //Define mac address of sender
-#define MAC_ADDRESS "98:76:B6:11:9b:17"
+#define MAC_ADDRESS "xx:xx:xx:xx:xx"
 
 //Set pin for the LoRa module
 #define RFM95_CS 8
